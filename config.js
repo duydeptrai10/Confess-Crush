1f50b87f4b0b33eee007676d9ef7e9c30dbfebbd
duydeptrai10 <106880499+duydeptrai10@users.noreply.github.com>
2022-06-05 00:25:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "xuanmaidangyeu",
+    introTitle: 'Babe à❤️',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Công chúa của anh ơi,đừng giận anh nữa nhaaa 🥰',
+    desc: 'Yêu em lắm íiiii ',
+    btnYes: 'Đương nhiên gòi sao mà giận anh được hihi😘',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu một người dễ thương như anh chứ ^^',
+    btnReply: 'Do you love me? (Yes i do or No',
+    reply: 'Yes',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Sáng mai, anh qua đón công chúa đi ăn nha🥰.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'M' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
